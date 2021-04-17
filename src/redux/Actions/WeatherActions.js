35e575c722f62e115e5a2e62681867aa4785dda9
dishaben9 <dishaben9@gmail.com>
2021-04-17 09:19:57 +0530

    changed API key to constants

--- src/redux/Actions/WeatherActions.js
+++ src/redux/Actions/WeatherActions.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import {LOCATION, STORE_CITIES} from '../Types';
 import Geolocation from '@react-native-community/geolocation';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {MAP_API_KEY} from '../../helpers/constants';
 
 let reqHeader = Object.assign({
   Accept: 'application/json',
@@ -12,7 +13,7 @@ export const getCities = () => {
   return dispatch => {
     return axios
       .get(
-        'http://api.openweathermap.org/data/2.5/find?lat=23.68&lon=90.35&cnt=50&appid=ffb8c3decb6e7b68c36aca2d55c410ac',
+        `http://api.openweathermap.org/data/2.5/find?lat=23.68&lon=90.35&cnt=50&appid=${MAP_API_KEY}`,
         reqHeader,
       )
       .then(res => {
@@ -34,7 +35,7 @@ export const getCityData = (lat, lon) => {
   return () => {
     return axios
       .get(
-        `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=ffb8c3decb6e7b68c36aca2d55c410ac`,
+        `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${MAP_API_KEY}`,
         reqHeader,
       )
       .then(res => {
